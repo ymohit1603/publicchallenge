104b26ed0fa4a638f3a5af97f3e7c0d9baefcfee
Mohit yadav <ymohit1603@gmail.com>
2025-08-21 19:16:21 +0530

    fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,9 @@
-import { PrismaClient } from './generated/prisma'
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient()
-// use `prisma` in your application to read and write data in your DB
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
